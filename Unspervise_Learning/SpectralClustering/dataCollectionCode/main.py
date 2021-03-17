9c7ab6338a9056d2ad37e93654979ee0439e618a
sd2beatles <53164959+sd2beatles@users.noreply.github.com>
2021-03-18 03:55:17 +0900

    Update main.py

--- Unspervise_Learning/SpectralClustering/dataCollectionCode/main.py
+++ Unspervise_Learning/SpectralClustering/dataCollectionCode/main.py
@@ -1,7 +1,25 @@
 
+def getArtists():
+    #collect artists from the outside source
+    kpops=pd.read_csv("/kpop_idols.xls")
+    k_artists=kpops[kpops.Group.notnull()].loc[:,"Group"]
+    k_artists=list(set([artist.strip()+",KOR" for artist in k_artists]))
+    
+    artists="/data.csv"
+    worlds=pd.read_csv(artists)
+    world_artists=worlds.artists.map(lambda x:filtering(x))
+    random_numbers=sorted(np.random.choice(1749,500,replace=False))
+    world_artists=world_artists[random_numbers].tolist()
+    artists=world_artists+k_artists
+    return k_artists
+
+
+
+
+
 def main():
-    client_id="161975af6e9c43eaa63de06b51331fdb"
-    client_secret="31704bd491a849d6bc440204d21a4776"
+    client_id="replace_me"
+    client_secret="replace_me"
     token=getToken(client_id,client_secret)
     artists=getArtists()
     records=[]
