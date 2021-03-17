470965ae4361eed4a751558b46ca3a328b091663
sd2beatles <53164959+sd2beatles@users.noreply.github.com>
2021-03-18 04:05:21 +0900

    Update spotify.py

--- Unspervise_Learning/SpectralClustering/dataCollectionCode/spotify.py
+++ Unspervise_Learning/SpectralClustering/dataCollectionCode/spotify.py
@@ -7,33 +7,6 @@ import pandas as pd
 import ast
 import numpy as np
 
-
-
-
-
-def getHeader(client_id,client_secret):
-    encoded=base64.b64encode("{}:{}".format(client_id,client_secret).encode("utf-8")).decode("ascii")
-    header={"Authorization":"Basic {}".format(encoded)}
-    return header
-
-def getToken(client_id,client_secret):
-    header=getHeader(client_id,client_secret)
-    payload={"grant_type":"client_credentials"}
-    endpoint="https://accounts.spotify.com/api/token"
-    raw=requests.post(endpoint,headers=header,data=payload)
-    
-    if raw.status_code==400:
-        raise ValueError("Error due to malforemd syntax")
-    elif raw.status_code==409 or raw.status_code==200:
-        if raw.status_code==409:
-            retry=json.loads(raw.headers)['Retry-After']
-            time.sleep(retry)
-            raw=requests.get(endpoint,headers=header,data=payload)
-        token=json.loads(raw.text)['access_token']
-        return {"Authorization":"Bearer {}".format(token)}
-
-      
-      
 class Spotify(object):
     def __init__(self,record,header,country):
         self.record=record
